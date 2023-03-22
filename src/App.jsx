342bcba2bf64bcb29557839110fe78cb12270191
juankhernadez83 <juankhernandez83@gmail.com>
2023-03-22 00:23:16 -0600

    Update App.jsx
    se agrega navbar dando Bienvenida con el nombre

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Lista from "./componentes/Lista"
 import ListaRicks from "./componentes/ListaRicks"
+import Navbar from './componentes/Navbar'
 
 function App() {
 
@@ -8,8 +9,7 @@ function App() {
   return (
     <div>
       <h1 className='text-center'>Rick and Morty</h1>
-
-   
+      <Navbar name="Kenia Paiz" />    
       <ListaRicks />
       <Lista/>
 
